e95f2b4c880c96923a3b9450c5f7d378650a02fe
AriannaRonci <75080239+AriannaRonci@users.noreply.github.com>
2021-02-25 09:47:41 +0100

    Update Stats.java

--- TicketMasterUS/src/main/java/it/univpm/TicketMasterUS/Utils/Statistics/Stats.java
+++ TicketMasterUS/src/main/java/it/univpm/TicketMasterUS/Utils/Statistics/Stats.java
@@ -1,5 +1,11 @@
 package it.univpm.TicketMasterUS.Utils.Statistics;
 
+import org.json.simple.JSONObject;
+
 public interface Stats {
 
+	public abstract int CalcoloTot();
+	public abstract JSONObject CalcoloperGenere();
+
 }
+
