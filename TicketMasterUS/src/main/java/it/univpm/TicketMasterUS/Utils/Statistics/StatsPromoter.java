eda309d67ab21dd274c546c9ce7237b52ad907a3
AriannaRonci <75080239+AriannaRonci@users.noreply.github.com>
2021-02-25 09:49:31 +0100

    Update StatsPromoter.java

--- TicketMasterUS/src/main/java/it/univpm/TicketMasterUS/Utils/Statistics/StatsPromoter.java
+++ TicketMasterUS/src/main/java/it/univpm/TicketMasterUS/Utils/Statistics/StatsPromoter.java
@@ -1,5 +1,43 @@
 package it.univpm.TicketMasterUS.Utils.Statistics;
 
+import java.util.Vector;
+
+import org.json.simple.JSONObject;
+
+import it.univpm.TicketMasterUS.Models.Event;
+import it.univpm.TicketMasterUS.Models.Promoter;
+
 public class StatsPromoter implements Stats {
+
+	public Vector<Event> events_stats= new Vector<>();
+
+	private String id_promoter;
+
+	public StatsPromoter(Vector<Event> e, String id_promoter) {
+		super();
+		this.id_promoter = id_promoter;
+		boolean daAggiungere = false;
+
+		//costruisco il mio vettore di eventi selezionando solo quelli che hanno il promoter specificato
+		for(Event event: e) {
+			for(Promoter p: event.getPromoters()){
+				if(p.getID().equals(id_promoter)) 
+					daAggiungere = true;
+			}
+			if(daAggiungere) 
+				events_stats.add(event);
+		}
+	}
+
+	public int CalcoloTot() {
+
+		return 0;
+	}
+
+
+	public JSONObject CalcoloperGenere() {
+
+		return null;
+	}
 
 }
