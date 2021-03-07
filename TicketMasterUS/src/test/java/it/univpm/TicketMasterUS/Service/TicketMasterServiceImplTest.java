0f46639c50e91a0a80f16ff4120b67ce4d4a377e
Alice Moretti <morettialice@outlook.it>
2021-03-07 15:42:19 +0100

    Test

---
+++ TicketMasterUS/src/test/java/it/univpm/TicketMasterUS/Service/TicketMasterServiceImplTest.java
@@ -0,0 +1,47 @@
+package it.univpm.TicketMasterUS.Service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Vector;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import it.univpm.TicketMasterUS.Exceptions.WrongPeriodException;
+import it.univpm.TicketMasterUS.Exceptions.WrongStateCodeException;
+
+public class TicketMasterServiceImplTest {
+	
+	private TicketMasterServiceImpl service;
+	private String stateCode;
+	
+	/**
+	 * Inizializza i componenti che verranno utilizzati per testare i metodi
+	 * @throws Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception{
+		service = new TicketMasterServiceImpl();
+		stateCode = "LA";
+	}
+	
+	/**
+	 * Distrugge ciò che è stato inizializzato nel metodo setUp
+	 * @throws Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception{
+		
+	}
+	
+	/**
+	 * Test che verifica se viene generata correttamente l'eccezione WrongStateCode
+	 */
+	@Test
+	@DisplayName("Corretta generazione dell'eccezione WrongStateCodeException")
+	void stateCodeTest() {		
+		assertThrows(WrongStateCodeException.class, () -> service.getEvents(stateCode));
+	}
+}
---
+++ TicketMasterUS/src/test/java/it/univpm/TicketMasterUS/Utils/Statistics/StatsStateTest.java
@@ -0,0 +1,47 @@
+package it.univpm.TicketMasterUS.Utils.Statistics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Vector;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import it.univpm.TicketMasterUS.Models.Event;
+
+public class StatsStateTest {
+	Vector<Event> event;
+	StatsState stats;
+	int num;
+
+	/**
+	 * Inizializza i componenti che verranno utilizzati per testare i metodi
+	 * @throws Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception{
+		event = new Vector<>();
+		stats = new StatsState(event);
+	}
+
+	/**
+	 * Distrugge ciò che è stato inizializzato nel metodo setUp
+	 * @throws Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception{
+
+	}
+
+	/**
+	 * Test che verifica il corretto funzionamento del metodo calcoloTot
+	 */
+	@Test
+	@DisplayName("Corretto funzionamento del metodo calcoloTot")
+	void infoPromoterTest() {
+		num = stats.calcoloTot();
+		assertEquals(num,0);
+	}
+}
